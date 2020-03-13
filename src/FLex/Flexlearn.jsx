a8b5776d37714fd74976c7c650fd1b5ede6e6235
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-03-13 22:04:52 +0300

    ssls

--- src/FLex/Flexlearn.jsx
+++ src/FLex/Flexlearn.jsx
@@ -25,152 +25,160 @@ const Flex1 = () => {
     return (
         <div className={s.main}>
 
-        
-                  <div className={s.mainHeader}>
+
+            <div className={s.mainHeader}>
 
                 <div className={s.header}>
 
-                    <img onClick={()=> settoggle(!toggle)} className={s.logoheader} src={leftbtn} alt="" />
-                    
-              
+                    <img onClick={() => settoggle(!toggle)} className={s.logoheader} src={leftbtn} alt="" />
 
 
 
-                                <div className={s.logo2}>   Россия</div>
-                                <img src={ignlogo} />
-                            </div>
-                            <div className={s.Header2}>
-                                <button> Вход  </button>
-                                <img src={searcjLogo} />
-                                {effect ?
-                                    <Spring
-                                        from={{ transform: 'rotate(-270deg)' }}
-                                        to={{ transform: 'rotate(-90deg)' }}
-
-                                        config={{ duration: 300 }}
-                                    >
-                                        {propss =>
-
-                                            <div style={propss} className={s.text1}> > </div>}
-                                    </Spring>
-                                    :
-
-                                    <div onMouseEnter={effects} className={s.text1}> > </div>}
 
 
-                                <div style={paint} onMouseEnter={effects} className={s.text}>More</div>
-                                <div className={s.text}>3DS</div>
-                                <div className={s.text}>XBOX One</div>
-                                <div className={s.text}>Switch</div>
-                                <div className={s.text}>PS4</div>
-                                <div className={s.text}>PC</div>
-                            </div>
-                            <div onMouseLeave={ckeareffects} style={show} className={s.hiddenhedder}>
-                                <div className={s.maincolons}>
-                                    <div className={s.colons1}>
-                                        <p> <span> IGN RUSSIA</span>
-                                        </p>
-                                        <p>Об IGN Russia
-                        </p>
-                                        <p>Команда сайта
-                        </p>
-                                        <p>Рекламодателям
-                        </p>
-                                        <p>Пользовотельское
-                        </p>
-                                        <p>Конфиденциальность
-                        </p>
-                                        <p>Правила
-                        </p>
-                                        <p>RSS
-                        </p>
-                                        <p>О нашей системе
-                        </p>
-
-                                    </div>
-                                    <div className={s.colons}>
-
-                                    </div>
-                                    <div className={s.colons}>
-
-                                        <p><span> SOCIAL </span>
-                                        </p>
-                                        <p>Facebook
-                        </p>
-                                        <p>Twitter
-                        </p>
-                                    </div>
-
-                                    <div className={s.colons}>
-                                        <p> <span> ENTERTAINMENT </span>
-                                        </p>
-                                        <p>Видео
-                        </p>
-                                        <p>Рецензии
-                        </p>
-                                        <p>Новости
-                        </p>
-                                        <p>Спецы
-                        </p>
-                                        <p>Превью
-                        </p>
-                                        <p>Фильмы
-                        </p>
-                                        <p>Железо
-                        </p>
-                                        <p>Игры
-                        </p>
-
-                                    </div>
-                                    <div className={s.colons}>
-                                        <p><span> GAMING </span>
-                                        </p>
-                                        <p>PC
-                        </p>
-                                        <p>PS4
-                        </p>
-                                        <p>XBOX One
-                        </p>
-                                        <p>PS3
-                        </p>
-                                        <p>3DS
-                        </p>
-                                        <p>PS Vita
-                        </p>
-                                        <p>Switch
-                        </p>
-                                        <p>Xbox 360
-                        </p>
-                                        <p>Wii U
-                        </p>
-                                    </div>
-                                </div>
-                            </div>
+                    <div className={s.logo2}>   Россия</div>
+                    <img src={ignlogo} />
                 </div>
-               
-                <Transition
-                        items={toggle}
-                        from={{ opacity: 1, width: '0%' }}
-                        enter={{ opacity: 1, width: '100%'  }}
-                        leave={{ opacity: 1, width: '0%'}}
-                        config={{ duration: 150 }}>
+                <div className={s.Header2}>
+                    <button> Вход  </button>
+                    <img src={searcjLogo} />
+                    {effect ?
+                        <Spring
+                            from={{ transform: 'rotate(-270deg)' }}
+                            to={{ transform: 'rotate(-90deg)' }}
 
-                        {toggle => toggle && (propss =>
-                            <div className={s.effectbar} style={propss} >
-                             
-                              
-                       <span onClick={()=> settoggle(!toggle)}  className={s.closebtn}>X </span> 
-                         
-                                </div>)}
+                            config={{ duration: 300 }}
+                        >
+                            {propss =>
 
-                                </Transition >
+                                <div style={propss} className={s.text1}> > </div>}
+                        </Spring>
+                        :
 
-                   <News />
-                   <News />
+                        <div onMouseEnter={effects} className={s.text1}> > </div>}
 
 
-
+                    <div style={paint} onMouseEnter={effects} className={s.text}>More</div>
+                    <div className={s.text}>3DS</div>
+                    <div className={s.text}>XBOX One</div>
+                    <div className={s.text}>Switch</div>
+                    <div className={s.text}>PS4</div>
+                    <div className={s.text}>PC</div>
                 </div>
+                <div onMouseLeave={ckeareffects} style={show} className={s.hiddenhedder}>
+                    <div className={s.maincolons}>
+                        <div className={s.colons1}>
+                            <p> <span> IGN RUSSIA</span>
+                            </p>
+                            <p>Об IGN Russia
+                        </p>
+                            <p>Команда сайта
+                        </p>
+                            <p>Рекламодателям
+                        </p>
+                            <p>Пользовотельское
+                        </p>
+                            <p>Конфиденциальность
+                        </p>
+                            <p>Правила
+                        </p>
+                            <p>RSS
+                        </p>
+                            <p>О нашей системе
+                        </p>
+
+                        </div>
+                        <div className={s.colons}>
+
+                        </div>
+                        <div className={s.colons}>
+
+                            <p><span> SOCIAL </span>
+                            </p>
+                            <p>Facebook
+                        </p>
+                            <p>Twitter
+                        </p>
+                        </div>
+
+                        <div className={s.colons}>
+                            <p> <span> ENTERTAINMENT </span>
+                            </p>
+                            <p>Видео
+                        </p>
+                            <p>Рецензии
+                        </p>
+                            <p>Новости
+                        </p>
+                            <p>Спецы
+                        </p>
+                            <p>Превью
+                        </p>
+                            <p>Фильмы
+                        </p>
+                            <p>Железо
+                        </p>
+                            <p>Игры
+                        </p>
+
+                        </div>
+                        <div className={s.colons}>
+                            <p><span> GAMING </span>
+                            </p>
+                            <p>PC
+                        </p>
+                            <p>PS4
+                        </p>
+                            <p>XBOX One
+                        </p>
+                            <p>PS3
+                        </p>
+                            <p>3DS
+                        </p>
+                            <p>PS Vita
+                        </p>
+                            <p>Switch
+                        </p>
+                            <p>Xbox 360
+                        </p>
+                            <p>Wii U
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Transition
+                items={toggle}
+                from={{ opacity: 1, right: 500 }}
+                enter={{ opacity: 1, right: 0 }}
+                leave={{ opacity: 1, right: 500 }}
+                config={{ duration: 150 }}>
+
+                {toggle => toggle && (propss =>
+                    <div className={s.effectbar} style={propss} >
+
+                        <div className={s.titlehbar}> <span> IGN Russia </span> </div>
+                        <Transition
+                            items={toggle}
+                            from={{ opacity: 0,marginLeft: '85%' }}
+                            enter={{ opacity: 1,marginLeft: '85%' }}
+                            leave= {{opacity: 0}}
+                            config={{ duration: 550 }}>
+
+                            {toggle => toggle && (propss => <span style={propss} onClick={() => settoggle(!toggle)} className={s.closebtn}> X </span> )}
+                           </Transition >
+
+                    </div>)}
+
+            </Transition >
+
+            <News />
+            <News />
+
+
+
+        </div>
 
 
 
